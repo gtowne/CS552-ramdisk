9dc7c6160af0943d52179f093e537d18006091cb
Diane Hirsh Theriault <deht@csa2.bu.edu>
2010-11-30 18:36:51 -0500

    IndexNode struct definition

--- inode.h
+++ inode.h
@@ -10,15 +10,18 @@
 Luis Carrasco, Diane Theriault, Gordon Towne
 Ramdisk - Project 3 - CS552
 
-BITMAP:
-This implements the bitmap part of the assignment
+INODE
+This implements the INode part of the assignment
 
 */
 
 #ifndef INODE_H_
 #define INODE_H_
 
+//I think this isn't defined till you include one of the cstdlib headers
 #define NULL 0x0
+
+
 #define BLOCK_SIZE 256
 
 //dummy struct so I can just write something
@@ -51,23 +54,8 @@ struct IndexNode
   char freeBytes[16]; //padding to make the struct the right size
 };
 
-void inode_init(IndexNode* iNode)
-{
-  iNode->type = IndexNode::UNALLOCATED;
-  iNode->size = 0;
-  for(int i=0; i<IndexNodeDirectPointers; i++)
-  {
-    iNode->directPointer[i] = NULL;
-  }
-  for(int i=0; i<IndexNodeIndirectPointers; i++)
-  {
-    iNode->indirectPointer[i] = NULL;
-  }
-  for(int i=0; i<IndexNodeDoublePointers; i++)
-  {
-    iNode->doubleIndirectPointer[i] = NULL;
-  }
-}
+void inode_init(IndexNode* iNode);
+
 
 /**
 @param[in]  IndexNode iNode  the index node for the file
@@ -75,69 +63,19 @@ void inode_init(IndexNode* iNode)
 @param[out] int oBlockOffset how many bytes into the block you should start
 */
 Block* inode_get_block_for_byte_index(IndexNode* iNode, 
-                                      int address, int* oBlockOffset=NULL)
-{
-  
-}
-
+                                      int address, int* oBlockOffset=NULL);
 
 /**
 @param[in]  IndexNode iNode    the index node for the file
 @param[in]  Block* iBlock      the block to add
-@param[in]  RAMDisk iRamDisk   the Ramdisk structure itself, (in case you need to allocate a new block
+@param[in]  RAMDisk iRamDisk   the Ramdisk structure itself, 
+                               (in case you need to allocate a new block
                                for an indirect node, etc.)
 */
-void inode_add_block_to_file(IndexNode* iNode, Block* iBlock, RAMDisk* iRamDisk)
-{
-  //find the next place to add a block.
-  // if it's in the direct pointers, you are done
-  // if it's in the indirect pointer
-  //    if the pointer is null, allocate a block for the pointers
-  //    if the block has space
-  //      add the pointer to the block
-  //    if there's no space, resort to the double pointers
-  // if it's in the double pointer
-  //    if the pointer is null, allocate a block for the indirect pointers
-  //       also allocate a block of pointers for the first indirect pointer to point to
-  //    walk through the tables for each indirect pointer, looking for space
-  //    if you find a slot in a block, you are done
-  //    if the table for the last allocated indirect pointer is full, 
-  //       allocate a new table for the next indirect pointer
-  //
-  //Phew!
-  //
-}
+void inode_add_block_to_file(IndexNode* iNode, 
+			     Block* iBlock, RAMDisk* iRamDisk);
 
-
-Block* ramdisk_allocate_new_block_for_file(RAMDisk*, IndexNode*)
-{
-  //allocate a new block
-  //update the IndexNode
-}
-
-Block* ramdisk_allocate_new_block(RAMDisk*)
-{
-  //find an available block
-  //mark it used
-  //return the pointer/index
-}
-
-// ramdisk create file
-void ramdisk_create_file()
-{
-  // make an entry in the directory
-  // grab an iNode
-  // grab an initial block
-}
-
-#include <iostream>
-using namespace std;
-
-/*
-int main()
-{
-  cout<<sizeof(IndexNode)<<endl;
-}
-*/
+//@TODO: NEED ALLOCATE BLOCK FUNCTION
+Block* ramdisk_allocate_block(RAMDisk* iRamDisk);
 
 #endif
